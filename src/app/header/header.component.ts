514525ebd5e0839697c857f85280be64c8884808
Maymaher <mai.maher003@gmail.com>
2021-06-20 21:39:22 -0400

    feature(feat-authentication) modifying authentication config

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { Headers, RequestOptions } from '@angular/http';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  books: any;
+  constructor(private http: HttpClient, private router: Router) { }
+  
+  ngOnInit() {
+    var headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + localStorage.getItem("token"));
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+    // let httpOptions = {
+      
+    //   // headers: new HttpHeaders({ "Authorization": localStorage.getItem('jwtToken') })
+    // };
+    // this.http.get('/student/student', options).subscribe(data => {
+    //   this.books = data;
+    //   console.log(this.books);
+    // }, err => {
+    //   if(err.status === 401) {
+    //     this.router.navigate(['login']);
+    //   }
+    // });
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout() {
+    localStorage.removeItem('jwtToken');
+    this.router.navigate(['login']);
   }
 
 }
