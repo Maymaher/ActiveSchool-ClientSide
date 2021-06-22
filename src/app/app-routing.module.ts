18662669a669d47fd83eec3ca03b6da3a41eda8d
Maymaher <mai.maher003@gmail.com>
2021-06-21 23:15:01 -0400

    removing comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,38 +11,3 @@ const routes: Routes = [
 
 export const appRoutingModule = RouterModule.forRoot(routes);
 
-
-
-
-
-
-// import { RouterModule, Routes } from '@angular/router';
-// import { HeaderComponent } from './header/header.component';
-// import { LoginComponent } from './login/login.component';
-// import { RegisterComponent } from './register/register.component';
-// const routes: Routes = [
-//   {
-//     path: 'books',
-//     component: HeaderComponent,
-//     data: { title: 'Book List' }
-//   },
-//   {
-//     path: 'login',
-//     component: LoginComponent,
-//     data: { title: 'Login' }
-//   },
-//   {
-//     path: 'signup',
-//     component: RegisterComponent,
-//     data: { title: 'Sign Up' }
-//   },
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(
-//     routes,
-//     { enableTracing: true } // <-- debugging purposes only
-//   )],
-//   exports: [RouterModule],
-// })
-// export class AppRoutingModule {}
