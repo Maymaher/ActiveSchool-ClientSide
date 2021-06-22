91c82afa5276b55fdee0d8f68d888d1b72fd7487
Maymaher <mai.maher003@gmail.com>
2021-06-21 22:50:56 -0400

    feature(feat-authentication) updating auth gard login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import { UserService } from '../services/user.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    private _userService: UserService,
+    private _router :Router
+  ){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+      if (this._userService.isLoggedIn()) {
+        return true;
+      }
+  
+      this._router.navigate([''], { queryParams: { returnUrl: state.url }} );
+      return false;
+    }
   
 }
