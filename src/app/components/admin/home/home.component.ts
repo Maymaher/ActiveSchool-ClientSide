af3b8c5b8dd9c00d0ebd6b09386c5566c414369d
Maymaher <mai.maher003@gmail.com>
2021-06-23 09:59:13 -0400

    feature(feat-admin-page)adding get students data method

--- src/app/components/admin/home/home.component.ts
+++ src/app/components/admin/home/home.component.ts
@@ -1,15 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeAdminComponent implements OnInit {
+   student:boolean=true;
+   teacher:boolean=false;
 
-  constructor() { }
+
+  constructor( private _router :Router,CommonModule:CommonModule) {
+   this.student=true;
+   this.teacher=false;
+
+   }
 
   ngOnInit(): void {
-  }
+    console.log("s:"+this.student)
 
+  }
+  btnClick () {
+    this._router.navigateByUrl('/register');
+};
+
+getStudentData(){
+  this.student=true;
+  this.teacher=false;
+  console.log("in function student"+this.student,this.teacher)
+
+}
+
+getTeacherData(){
+  this.student=false;
+  this.teacher=true;
+  console.log("in function student"+this.student,this.teacher)
+
+
+}
 }
