72d73ca78ff1af67e19d29524a3ecfe35eb35fc5
Maymaher <mai.maher003@gmail.com>
2021-06-24 23:31:43 -0400

    feature(featadmin-pages) adding teacher details and delete user route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeAdminComponent } from './components/admin/home/home.component';
 import { SignUpComponent } from './components/admin/sign-up/sign-up.component';
+import { TeacherDetailsComponent } from './components/admin/teacher-details/teacher-details.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +10,8 @@ const routes: Routes = [
     {path:'header',component:HeaderComponent},
     { path: 'register', component: SignUpComponent },
     { path: 'admin/home', component: HomeAdminComponent ,canActivate: [AuthGuard]},
+    { path: 'admin/teacherDetails/:id', component: TeacherDetailsComponent ,canActivate: [AuthGuard]},
+
     { path: '', component: LoginComponent },
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     // otherwise redirect to home
