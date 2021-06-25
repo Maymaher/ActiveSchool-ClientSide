9611687be1d0ecfa51a370ebaa2aa2c058544efc
Maymaher <mai.maher003@gmail.com>
2021-06-24 23:33:06 -0400

    feature(featadmin-pages) fetching students and teachers data

--- src/app/components/admin/home/home.component.ts
+++ src/app/components/admin/home/home.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { StudentService } from 'src/app/services/student.service';
+import { TeacherService } from 'src/app/services/teacher.service';
+import { UserService } from '../../../services/user.service';
 
 
 @Component({
@@ -12,29 +16,72 @@ export class HomeAdminComponent implements OnInit {
    student:boolean=true;
    teacher:boolean=false;
 
+   teachersData:any;
+   studentsData:any;
 
-  constructor( private _router :Router,CommonModule:CommonModule) {
+
+  constructor( private _router :Router,CommonModule:CommonModule,
+    private _teacherService: TeacherService,
+    private _studentService:StudentService,
+    private _userService:UserService,
+    private _flash:FlashMessagesService
+    ) {
    this.student=true;
    this.teacher=false;
 
    }
 
   ngOnInit(): void {
-    console.log("s:"+this.student)
+   this.getStudentsData()
+   this.getTeacherData()
 
   }
-  btnClick () {
-    this._router.navigateByUrl('/register');
-};
 
-getStudentData(){
+  getTeacherData(){
+    this._teacherService.getTeachers().subscribe(
+      resp=>{
+        this.teachersData=resp;
+      }
+    )
+  }
+
+  getStudentsData(){
+    this._studentService.getstudents().subscribe(
+      resp=>{
+        // console.log(resp);
+        this.studentsData=resp;
+        console.log(this.studentsData);
+        
+      }
+    )
+  }
+
+//   btnClick () {
+//     this._router.navigateByUrl('/register');
+// };
+
+deleteUser(user_id:any){
+  this._userService.deleteUser(user_id).subscribe(
+    resp=>{
+      if (!resp.success) {
+        this._flash.show(resp.message, { cssClass: 'alert-danger' });
+        return false;
+      }
+   
+        this._flash.show("user deleted", { cssClass: 'alert-success' });
+
+     return  this._router.navigate(['/admin/home']);
+    }
+  )
+}
+
+getStudent(){
   this.student=true;
   this.teacher=false;
-  console.log("in function student"+this.student,this.teacher)
 
 }
 
-getTeacherData(){
+getTeacher(){
   this.student=false;
   this.teacher=true;
   console.log("in function student"+this.student,this.teacher)
