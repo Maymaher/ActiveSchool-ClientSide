6232bdfca1f34f434ee0b8908c925b4506d042f2
Maymaher <mai.maher003@gmail.com>
2021-06-29 17:37:46 -0400

    feature(feat-admin-page) adding edit user and edit course routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './components/admin/courses/courses.component';
+import { EditCourseComponent } from './components/admin/edit-course/edit-course.component';
+import { EditUserComponent } from './components/admin/edit-user/edit-user.component';
 import { HomeAdminComponent } from './components/admin/home/home.component';
 import { SignUpComponent } from './components/admin/sign-up/sign-up.component';
 import { TeacherDetailsComponent } from './components/admin/teacher-details/teacher-details.component';
@@ -13,6 +15,10 @@ const routes: Routes = [
     { path: 'admin/home', component: HomeAdminComponent ,canActivate: [AuthGuard]},
     { path: 'admin/teacherDetails/:id', component: TeacherDetailsComponent ,canActivate: [AuthGuard]},
     { path: 'admin/teacherDetails/:id/:courseId', component: TeacherDetailsComponent ,canActivate: [AuthGuard]},
+    { path: 'admin/editUser/:id', component: EditUserComponent ,canActivate: [AuthGuard]},
+
+    { path: 'admin/editCourse/:id', component: EditCourseComponent ,canActivate: [AuthGuard]},
+
     { path: 'admin/courses', component: CoursesComponent ,canActivate: [AuthGuard]},
 
     { path: '', component: LoginComponent },
