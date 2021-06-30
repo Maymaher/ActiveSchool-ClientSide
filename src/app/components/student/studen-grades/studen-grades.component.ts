5da070d045d362a594816c2a112b861fce0497fa
emansoliman <emyrade29@gmail.com>
2021-06-30 16:45:03 -0400

    feat-Student:add function in StudentGradeComponent

--- src/app/components/student/studen-grades/studen-grades.component.ts
+++ src/app/components/student/studen-grades/studen-grades.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../../services/user.service';
+import { StudentService } from '../../../services/student.service';
+import { Router } from "@angular/router";
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-studen-grades',
   templateUrl: './studen-grades.component.html',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudenGradesComponent implements OnInit {
 
-  constructor() { }
+  grades:any=[{}]
+  constructor(
+    public authService: StudentService,
+    public router: Router,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this.authService.getAllGrates("60dbc7bb931cfd6e84cb72fe").subscribe(resp =>{
+
+      this.grades=resp;      
+      console.log(this.grades);
+      
+    })
+  
   }
 
 }
