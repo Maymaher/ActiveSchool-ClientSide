ac8a9508a613cc53e81e1901c6d8d105236d582c
emansoliman <emyrade29@gmail.com>
2021-06-30 15:04:09 -0400

    Feat-Student-Grades:create  path for student gradeComponent

---
+++ src/app/components/student/studen-grades/studen-grades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studen-grades',
+  templateUrl: './studen-grades.component.html',
+  styleUrls: ['./studen-grades.component.css']
+})
+export class StudenGradesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
