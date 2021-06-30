ae6f240e3d555ad8d29864fe998c397937bc3e91
Omnia <omniamostafa1998@gmail.com>
2021-06-30 19:53:39 +0200

    Adding exam component

---
+++ src/app/components/exam/exam/exam.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { ExamService } from '../../../services/exam.service';
+
+@Component({
+  selector: 'app-exam',
+  templateUrl: './exam.component.html',
+  styleUrls: ['./exam.component.css']
+})
+export class ExamComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute,private _userService :UserService,private _examService :ExamService) { }
+  
+  exam:any={};
+  examPath="http://localhost:3200/public/exams/";
+  fd=new FormData();
+  myfile:any=null;
+  eid:any;
+
+  ngOnInit(): void {
+    this.eid = this.activatedRoute.snapshot.paramMap.get('id');
+    this._examService.getSpecificExam(this.eid).subscribe(data =>{
+      this.exam=data;
+     // this.from=this.exams.from.slice(12, 24);
+      console.log(this.exam);
+    })
+    
+  }
+
+  getFile(event:any){
+    this.myfile=<File>event.target.files[0];
+    if(!this.myfile.name.match(/([a-zA-Z0-9\s_\\.\-\(\):])+(.doc|.docx|.pdf|.png|.svg|.jpg|.jpeg|.gif)$/)){
+      alert("Sorry, You Can't upload This Type");
+      console.log(this.myfile);
+      this.myfile=null;
+    }
+    else{
+      this.fd.append('file',this.myfile,this.myfile.name);
+      console.log(this.myfile);
+
+    }
+
+
+}
+UplaodExam(){
+  const exam={
+    
+    file:this.myfile,
+    
+
+  }
+  this._examService.uploadExamAnswer(this.eid,this.exam,this.myfile).subscribe(data=>{
+    console.log(data);
+   })
+ 
+}
+
+
+
+
+}
