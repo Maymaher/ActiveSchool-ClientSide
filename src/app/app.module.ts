006d57bef54b052b40e36c1d593ccb75602656d5
emansoliman <emyrade29@gmail.com>
2021-06-30 22:46:40 -0400

    feat-Student:add gradetable in StudentGradeComponent.Html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {SidebarModule} from 'ng-sidebar';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
