2e779f53b0ab58b47d800060e572d5c8856c39b8
Omnia <omniamostafa1998@gmail.com>
2021-07-01 11:06:15 +0200

    Adding homework upload function

--- src/app/services/homework.service.ts
+++ src/app/services/homework.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import * as AppUtil from '../common/app.util';
+import { User } from '../user';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Http } from '@angular/http';
+ 
+import { map } from "rxjs/operators";
+import { error } from '@angular/compiler/src/util';
 @Injectable({
   providedIn: 'root'
 })
 export class HomeworkService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
+
+//Upload homework file
+upload(id:any,homework:any,file:File)
+  {
+    const formData:FormData = new FormData();
+    formData.append('file',file)
+    
+
+    console.log(formData);
+   
+      return this._http.post('http://localhost:3200/homeworkUpload'+id, formData)
+      .pipe(map((resp:any)=> resp.json()));
+      
+    
+    
+
+
+  }
+
 }
