620c4019be4dfc3688755a385f4dfeb801ca055e
Maymaher <mai.maher003@gmail.com>
2021-07-01 00:17:34 -0400

    feature(feat-teacher-exam-grades)check type of current user is authorized to open courses component or not

--- src/app/components/admin/courses/courses.component.ts
+++ src/app/components/admin/courses/courses.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { CourseService } from '../../../services/course.service';
 import { LevelService } from '../../../services/level.service';
+import { UserService } from '../../../services/user.service';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -15,16 +16,20 @@ export class CoursesComponent implements OnInit {
 
   courses:any;
   levels:any;
+  CurrentUser:any;
 
   constructor(private courseService:CourseService,
     private _flash:FlashMessagesService,
     private _router :Router,
-    private levelService:LevelService
+    private levelService:LevelService,
+    private _userService:UserService
     ) { }
 
   ngOnInit(): void {
     this.getCourses()
     this.getLevels()
+    this.CurrentUser=this._userService.getCurrentUser();
+
   }
   getCourses(){
     this.courseService.getAllCourses().subscribe(
