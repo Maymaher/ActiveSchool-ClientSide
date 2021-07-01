9373661eb25d51345ec34cd3396aa6c706325fd0
Maymaher <mai.maher003@gmail.com>
2021-07-01 00:15:46 -0400

    feature(feat-teacher-exam-grades)adding students exam answers and students grades comonents in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {SidebarModule} from 'ng-sidebar';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { SidebarModule } from 'ng-sidebar';
 import { appRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './components/admin/courses/courses.component';
@@ -16,23 +16,26 @@ import { EditUserComponent } from './components/admin/edit-user/edit-user.compon
 import { HomeAdminComponent } from './components/admin/home/home.component';
 import { SignUpComponent } from './components/admin/sign-up/sign-up.component';
 import { TeacherDetailsComponent } from './components/admin/teacher-details/teacher-details.component';
+import { ExamComponent } from './components/exam/exam/exam.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AttendenceComponent } from './components/student/attendence/attendence.component';
 import { ProfileComponent } from './components/student/profile/profile.component';
-import {ProfileTeacherComponent} from './components/teacher/profile/profile.component';
 import { SchedularComponent } from './components/student/schedular/schedular.component';
+import { StudenthomeComponent } from './components/student/studenthome/studenthome.component';
+import { AddStudentsGradesComponent } from './components/teacher/add-students-grades/add-students-grades.component';
+import { ExamTeacherComponent } from './components/teacher/exam-teacher/exam-teacher.component';
+import { ProfileTeacherComponent } from './components/teacher/profile/profile.component';
+import { StudentsExamAnswersComponent } from './components/teacher/students-exam-answers/students-exam-answers.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ClassService } from './services/class.service';
 import { CourseService } from './services/course.service';
+import { ExamAnswerService } from './services/exam-answer.service';
 import { LevelService } from './services/level.service';
 import { StudentService } from './services/student.service';
 import { TeacherService } from './services/teacher.service';
 import { UserService } from './services/user.service';
-import { ExamTeacherComponent } from './components/teacher/exam-teacher/exam-teacher.component';
-import { ExamComponent } from './components/exam/exam/exam.component';
-import { StudenthomeComponent } from './components/student/studenthome/studenthome.component';
 
  
 
@@ -73,7 +76,10 @@ import { StudenthomeComponent } from './components/student/studenthome/studentho
         SchedularComponent,
         AttendenceComponent,
         StudenthomeComponent,
-        ProfileTeacherComponent
+        ProfileTeacherComponent,
+        StudentsExamAnswersComponent,
+        AddStudentsGradesComponent,
+        
     
        
     ],
@@ -84,7 +90,8 @@ import { StudenthomeComponent } from './components/student/studenthome/studentho
         ClassService,
         LevelService,
         CourseService,
-        StudentService
+        StudentService,
+        ExamAnswerService
     ],
     bootstrap: [AppComponent]
 })
