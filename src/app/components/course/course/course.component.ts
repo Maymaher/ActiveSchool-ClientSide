97f03b5a60347816cdf3c2b29a48df2058f8ab25
Omnia <omniamostafa1998@gmail.com>
2021-07-01 12:19:54 +0200

    Adding upload function

--- src/app/components/course/course/course.component.ts
+++ src/app/components/course/course/course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeworkService } from '../../../services/homework.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  fd=new FormData();
+  myfile:any=null;
+  cid:any="";
+  CurrentUser:any="";
 
-  constructor() { }
+  constructor(private _homeworkService :HomeworkService,private activatedRoute: ActivatedRoute,private _userService :UserService,private _flash :FlashMessagesService,) { 
+    
+  }
+
+  getFile(event:any){
+    this.myfile=<File>event.target.files[0];
+    if(!this.myfile.name.match(/([a-zA-Z0-9\s_\\.\-\(\):])+(.doc|.docx|.pdf|.png|.svg|.jpg|.jpeg|.gif)$/)){
+      alert("Sorry, You Can't upload This Type");
+      console.log(this.myfile);
+      this.myfile=null;
+    }
+    else{
+      this.fd.append('file',this.myfile,this.myfile.name);
+      console.log(this.myfile);
+
+    }
+  }
 
   ngOnInit(): void {
+    this.cid = this.activatedRoute.snapshot.paramMap.get('id');
+    this.CurrentUser=this._userService.getCurrentUser();
+      
+  }
+
+  UplaodHomework(){
+     if(this.myfile==null)
+    {
+      alert("Please, Enter a valid file")
+    }
+    else{
+      const homework={
+        file:this.myfile,
+        
+      }
+      this._homeworkService.upload(this.cid,homework,this.myfile).subscribe(data=>{
+        this._flash.show(data.message, { cssClass: 'alert-success'});
+      })
+     
+      
+  
+    }
   }
 
 }
